0f2ba400bbc1303d6f71d5d4e49a14b20f456073
EngineerTrooper <kontakt@engineertrooper.com>
2023-01-28 15:34:21 +0100

    adding README.md and systemCheck.ps1

---
+++ README.md
@@ -0,0 +1,10 @@
+# etPowerShell
+
+is a collection of PowerShell scripts to optimize, get information or automize tasks on a windows machine. Later also network and remote stuff on windows machines. 
+
+Linux is not in scope right now, because bash is I think a way better option on linux machines. But if you use powershell on linux, feel free to start a discussion with an issue or give feedback.
+
+## Scripts Description
+
+### systemCheck.ps1
+Is a collection of methods and functions to give a full overview of the machine, where it is executed. The script can take between 1 to 3 minutes to get all information ready. Make sure the destination directory exists on your system to save the report files!
---
+++ systemCheck.ps1
@@ -0,0 +1,70 @@
+Write-Host "`n################################################"
+Write-Host "file: systemCheck.ps1"
+$date = Get-Date
+Write-Host ("start at: {0}" -f $date)
+Write-Host "################################################`n"
+
+$savePath = ""
+While (!(Test-Path -Path $savePath))
+{
+    # if($savePath -eq "exit"){break}
+    $savePath = Read-Host -Prompt "Input directory path"
+    if((Test-Path -Path $savePath))
+    {
+        break
+    }
+    else
+    {
+        Write-Host "Err: Path doesn't exist $savePath"
+    }
+}
+
+Write-Host "`n`nCheck..."
+
+Write-Host " ExecutionPolicy"
+$currPath = Join-Path -path $savePath -childpath "ExecutionPolicy.txt"
+Get-ExecutionPolicy |
+  Out-File $currPath
+
+
+Write-Host " ComputerInfo"
+$currPath = Join-Path -path $savePath -childpath "\ComputerInfo.txt"
+Get-ComputerInfo |
+  Out-File $currPath
+
+
+Write-Host " Get-Command"
+$currPath = Join-Path -path $savePath -childpath "\Get-Command.txt"
+Get-Command |
+  Out-File $currPath
+
+
+Write-Host " Get InstalledSoftware"
+$currPath = Join-Path -path $savePath -childpath "\InstalledSoftware.txt"
+$pathsToInstalledSoftware=@(
+  'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\',
+  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+)
+foreach($path in $pathsToInstalledSoftware){
+  Get-ChildItem -Path $path | 
+    Get-ItemProperty | 
+      Select-Object DisplayName, Publisher, InstallDate, DisplayVersion, InstallLocation |
+        Format-Table -AutoSize |  
+          Out-File $currPath
+}
+
+
+Write-Host " powercfg get energyreport {`n"
+$currPath = Join-Path -path $savePath -childpath "\Powercfg-Energyreport.html"
+powercfg.exe -energy -output $currPath
+Write-Host "`n}`n"
+
+
+Write-Host " powercfg get batteryreport {`n"
+$currPath = Join-Path -path $savePath -childpath "\Powercfg-Batteryreport.html"
+powercfg.exe -batteryreport -output $currPath
+Write-Host "`n}`n"
+
+
+
+Write-Host ("`n`n finished.`n See results at {0}" -f $savePath)
